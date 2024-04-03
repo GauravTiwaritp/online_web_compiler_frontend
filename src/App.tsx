2fd9ce3c6b981da7e5d97efc5a4c68dc7be7bf69
GauravTiwaritp <gauravtiwari282001@gmail.com>
2024-04-03 20:55:02 +0530

    Day 1 on this project shadcn configured

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+function App() {
+  return (
+    <>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro aperiam
+      rerum officia corporis vero sed itaque alias assumenda quaerat
+      necessitatibus, ex hic in blanditiis possimus animi autem modi provident
+      facere.
+      <Button variant="destructive">test btn</Button>
+    </>
+  );
+}
+
+export default App;
